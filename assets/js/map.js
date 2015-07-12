a1857beda879b10afc6e4eef13a88f12f65a4194
robneu <rob@wpsitecare.com>
2015-07-11 23:07:29 -0500

    Cleaned up JS

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,62 +1,62 @@
-/* Frontend Javascript for Business Profile maps */
-jQuery(document).ready(function ($) {
 
-	// Load Google Maps API and initialize maps
-	var bp_map_script = document.createElement( 'script' );
-	bp_map_script.type = 'text/javascript';
-	bp_map_script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=bp_initialize_map';
-	document.body.appendChild( bp_map_script );
+function bpInitializeMap() {
+	'use strict';
 
-});
-
-function bp_initialize_map() {
-
-	var bp_maps = [];
-	var bp_info_windows = [];
-
-	jQuery( '.bp-map' ).each( function() {
-		var id = jQuery(this).attr( 'id' );
-		var data = jQuery(this).data();
+	jQuery( '.bp-map' ).each(function( $ ) {
+		var bpMaps        = [],
+			bpInfoWindows = [],
+			$that         = $( this ),
+			id            = $that.attr( 'id' ),
+			data          = $that.data(),
+			latLon, bpMapOptions, content, bpMapIframe;
 
 		// Google Maps API v3
-		if ( typeof data.lat !== 'undefined' ) {
-			var latlon = new google.maps.LatLng( data.lat, data.lon );
-			var bp_map_options = {
-				zoom: 15,
-				center: latlon,
+		if ( 'undefined' !== typeof data.lat ) {
+			latLon = new google.maps.LatLng( data.lat, data.lon );
+			bpMapOptions = {
+				zoom:   15,
+				center: latLon
 			};
 
-			bp_maps[ id ] = new google.maps.Map( document.getElementById( id ), bp_map_options );
-			
-			var content = '<div class="bp-map-info-window">' +
-				'<p><strong>' + data.name + '</strong></p>' + 
-				'<p>' + data.address + '</p>';
-				
-			if ( typeof data.phone !== 'undefined' ) {
+			bpMaps[ id ] = new google.maps.Map( document.getElementById( id ), bpMapOptions );
+
+			content = '<div class="bp-map-info-window">' + '<p><strong>' + data.name + '</strong></p>' + '<p>' + data.address + '</p>';
+			if ( 'undefined' !== typeof data.phone ) {
 				content += '<p>' + data.phone + '</p>';
 			}
-			content += '<p><a target="_blank" href="//maps.google.com/maps?saddr=current+location&daddr=' + encodeURIComponent( data.address ) + '">Get Directions</a></p>' +
-				'</div>';
-			
-			bp_info_windows[ id ] = new google.maps.InfoWindow({
-				position: latlon,
-				content: content,
-			});
-			bp_info_windows[ id ].open( bp_maps[ id ]);
+			content += '<p><a target="_blank" href="//maps.google.com/maps?saddr=current+location&daddr=' + encodeURIComponent( data.address ) + '">Get Directions</a></p>' + '</div>';
+
+			bpInfoWindows[ id ] = new google.maps.InfoWindow( {
+				position: latLon,
+				content:  content
+			} );
+			bpInfoWindows[ id ].open( bpMaps[ id ] );
 
 		// Google Maps iframe embed (fallback if no lat/lon data available)
-		} else if ( typeof data.address !== '' ) {
-			var bp_map_iframe = document.createElement( 'iframe' );
-			bp_map_iframe.frameBorder = 0;
-			bp_map_iframe.style.width = '100%';
-			bp_map_iframe.style.height = '100%';
+		} else if ( '' !== typeof data.address ) {
+			bpMapIframe = document.createElement( 'iframe' );
 
-			if ( typeof data.name !== '' ) {
+			bpMapIframe.frameBorder  = 0;
+			bpMapIframe.style.width  = '100%';
+			bpMapIframe.style.height = '100%';
+
+			if ( '' !== typeof data.name ) {
 				data.address = data.name + ',' + data.address;
 			}
-			bp_map_iframe.src = '//maps.google.com/maps?output=embed&q=' + encodeURIComponent( data.address );
+			bpMapIframe.src = '//maps.google.com/maps?output=embed&q=' + encodeURIComponent( data.address );
 
-			jQuery(this).html( bp_map_iframe );
+			$that.html( bpMapIframe );
 		}
 	});
 }
+
+/* Frontend Javascript for Business Profile maps */
+jQuery( document ).ready(function() {
+	// Load Google Maps API and initialize maps
+	var bpMapScript = document.createElement( 'script' );
+
+	bpMapScript.type = 'text/javascript';
+	bpMapScript.src  = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=bpInitializeMap';
+
+	document.body.appendChild( bpMapScript );
+});
