aaf226e3ef336ad45a0e934a34ca81e1c084a145
Nate Wright <natew@notthisway.com>
2016-05-10 14:27:40 +0100

    #8 Add scheduler to location edit screen

--- includes/class-custom-post-types.php
+++ includes/class-custom-post-types.php
@@ -296,7 +296,7 @@ if ( ! class_exists( 'bpfwpCustomPostTypes', false ) ) :
 				);
 				if ( $query->have_posts() ) {
 					while ( $query->have_posts() ) {
-						$query->the_post();
+						$query->next_post();
 						$pages[get_the_ID()] = get_the_title();
 					}
 				}
@@ -335,22 +335,97 @@ if ( ! class_exists( 'bpfwpCustomPostTypes', false ) ) :
 		}
 
 		/**
-		* Output the metabox HTML to define opening hours
-		*
-		* @since 1.1
-		*/
+		 * Output the metabox HTML to define opening hours
+		 *
+		 * @since 1.1
+		 */
 		public function print_opening_hours_metabox( $post ) {
 
-			// @todo this is just a placeholder. The scheduler component from
-			// simple-admin-pages will be used
+			$scheduler = $this->get_scheduler_meta_object( get_post_meta( $post->ID, 'opening_hours', true ) );
+
+			// Load required scripts and styles
+			wp_enqueue_style( 'bpfwp-admin-location-sap', BPFWP_PLUGIN_URL . '/lib/simple-admin-pages/css/admin.css' );
+			foreach( $scheduler->styles as $handle => $style ) {
+				wp_enqueue_style( $handle, BPFWP_PLUGIN_URL . '/lib/simple-admin-pages/' . $style['path'], $style['dependencies'], $style['version'], $style['media'] );
+			}
+			foreach( $scheduler->scripts as $handle => $script ) {
+				wp_enqueue_script( $handle, BPFWP_PLUGIN_URL . '/lib/simple-admin-pages/' . $script['path'], $script['dependencies'], $script['version'], $script['footer'] );
+			}
 			?>
 
 			<div class="bpfwp-meta-input bpfwp-meta-opening-hours">
-				<!-- nothing here yet -->
+				<?php $scheduler->display_setting(); ?>
 			</div>
 
 			<?php
 		}
+
+		/**
+		 * Get a modified Scheduler object from the Simple Admin Pages library
+		 *
+		 * This modified scheduler is used to display and sanitize a scheduler
+		 * component on the location post editing screen.
+		 *
+		 * @since 1.1
+		 * @see lib/simple-admin-pages/classes/AdminPageSetting.Scheduler.class.php
+		 */
+		public function get_scheduler_meta_object( $values = null ) {
+
+			require_once BPFWP_PLUGIN_DIR . '/includes/class-sap-scheduler-meta.php';
+			$scheduler = new bpfwpSAPSchedulerMeta(
+				array(
+					'page'          => 'dummy_page', // required but not used
+					'id'            => 'opening_hours',
+					'title'         => __( 'Opening Hours', 'business-profile' ),
+					'description'   => __( 'Define your weekly opening hours by adding scheduling rules.', 'business-profile' ),
+					'weekdays'      => array(
+						'monday'    => _x( 'Mo', 'Monday abbreviation', 'business-profile' ),
+						'tuesday'   => _x( 'Tu', 'Tuesday abbreviation', 'business-profile' ),
+						'wednesday' => _x( 'We', 'Wednesday abbreviation', 'business-profile' ),
+						'thursday'  => _x( 'Th', 'Thursday abbreviation', 'business-profile' ),
+						'friday'    => _x( 'Fr', 'Friday abbreviation', 'business-profile' ),
+						'saturday'  => _x( 'Sa', 'Saturday abbreviation', 'business-profile' ),
+						'sunday'    => _x( 'Su', 'Sunday abbreviation', 'business-profile' )
+					),
+					'time_format'   => _x( 'h:i A', 'Time format displayed in the opening hours setting panel in your admin area. Must match formatting rules at http://amsul.ca/pickadate.js/time.htm#formats', 'business-profile' ),
+					'date_format'   => _x( 'mmmm d, yyyy', 'Date format displayed in the opening hours setting panel in your admin area. Must match formatting rules at http://amsul.ca/pickadate.js/date.htm#formatting-rules', 'business-profile' ),
+					'disable_weeks' => true,
+					'disable_date'  => true,
+					'strings'       => array(
+						'add_rule'         => __( 'Add another opening time', 'business-profile' ),
+						'weekly'           => _x( 'Weekly', 'Format of a scheduling rule', 'business-profile' ),
+						'monthly'          => _x( 'Monthly', 'Format of a scheduling rule', 'business-profile' ),
+						'date'             => _x( 'Date', 'Format of a scheduling rule', 'business-profile' ),
+						'weekdays'         => _x( 'Days of the week', 'Label for selecting days of the week in a scheduling rule', 'business-profile' ),
+						'month_weeks'      => _x( 'Weeks of the month', 'Label for selecting weeks of the month in a scheduling rule', 'business-profile' ),
+						'date_label'       => _x( 'Date', 'Label to select a date for a scheduling rule', 'business-profile' ),
+						'time_label'       => _x( 'Time', 'Label to select a time slot for a scheduling rule', 'business-profile' ),
+						'allday'           => _x( 'All day', 'Label to set a scheduling rule to last all day', 'business-profile' ),
+						'start'            => _x( 'Start', 'Label for the starting time of a scheduling rule', 'business-profile' ),
+						'end'              => _x( 'End', 'Label for the ending time of a scheduling rule', 'business-profile' ),
+						'set_time_prompt'  => _x( 'All day long. Want to %sset a time slot%s?', 'Prompt displayed when a scheduling rule is set without any time restrictions', 'business-profile' ),
+						'toggle'           => _x( 'Open and close this rule', 'Toggle a scheduling rule open and closed', 'business-profile' ),
+						'delete'           => _x( 'Delete rule', 'Delete a scheduling rule', 'business-profile' ),
+						'delete_schedule'  => __( 'Delete scheduling rule', 'business-profile' ),
+						'never'            => _x( 'Never', 'Brief default description of a scheduling rule when no weekdays or weeks are included in the rule', 'business-profile' ),
+						'weekly_always'    => _x( 'Every day', 'Brief default description of a scheduling rule when all the weekdays/weeks are included in the rule', 'business-profile' ),
+						'monthly_weekdays' => _x( '%s on the %s week of the month', 'Brief default description of a scheduling rule when some weekdays are included on only some weeks of the month. %s should be left alone and will be replaced by a comma-separated list of days and weeks in the following format: M, T, W on the first, second week of the month', 'business-profile' ),
+						'monthly_weeks'    => _x( '%s week of the month', 'Brief default description of a scheduling rule when some weeks of the month are included but all or no weekdays are selected. %s should be left alone and will be replaced by a comma-separated list of weeks in the following format: First, second week of the month', 'business-profile' ),
+						'all_day'          => _x( 'All day', 'Brief default description of a scheduling rule when no times are set', 'business-profile' ),
+						'before'           => _x( 'Ends at', 'Brief default description of a scheduling rule when an end time is set but no start time. If the end time is 6pm, it will read: Ends at 6pm', 'business-profile' ),
+						'after'            => _x( 'Starts at', 'Brief default description of a scheduling rule when a start time is set but no end time. If the start time is 6pm, it will read: Starts at 6pm', 'business-profile' ),
+						'separator'        => _x( '&mdash;', 'Separator between times of a scheduling rule', 'business-profile' ),
+					),
+				)
+			);
+
+			if ( !empty( $values ) ) {
+				$scheduler->set_value( $values );
+			}
+
+			return $scheduler;
+		}
+
 
 		/**
 		 * Sanitize and save the post meta
@@ -388,7 +463,7 @@ if ( ! class_exists( 'bpfwpCustomPostTypes', false ) ) :
 				'phone'         => 'sanitize_text_field',
 				'contact_post'  => 'absint',
 				'contact_email' => 'sanitize_email',
-				'opening_hours' => 'sanitize_text_field', // @todo
+				'opening_hours' => array( $this, 'sanitize_opening_hours' ),
 			);
 
 			foreach( $post_meta as $key => $callback ) {
@@ -406,6 +481,21 @@ if ( ! class_exists( 'bpfwpCustomPostTypes', false ) ) :
 
 			return $post_id;
 		}
+
+		/**
+		 * Sanitize opening hours
+		 *
+		 * This is a wrapper for the sanitization callback in the Scheduler
+		 * component of Simple Admin Pages
+		 *
+		 * @see lib/simple-admin-pages/classes/AdminPageSetting.Scheduler.class.php
+		 * @since 1.1
+		 */
+		public function sanitize_opening_hours( $values ) {
+			$scheduler = $this->get_scheduler_meta_object( $values );
+			return $scheduler->sanitize_callback_wrapper( $values );
+		}
+
 
 	}
 endif;
