b2696c946500593927165728ba2777442287bebf
Nate Wright <natew@notthisway.com>
2017-03-14 14:00:33 +0000

    Fix grunt makepot task

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,21 +27,12 @@ module.exports = function(grunt) {
 		makepot: {
 			target: {
 				options: {
-					cwd: 'business-profile/',                          // Directory of files to internationalize.
-					domainPath: 'languages',                   // Where to save the POT file.
-					exclude: [],                      // List of files or directories to ignore.
-					include: [],                      // List of files or directories to include.
-					i18nToolsPath: '/media/Storage/projects/wordpress/trunk/tools/i18n',                // Path to the i18n tools directory.
-					mainFile: 'business-profile.php',                     // Main project file.
-					potComments: '',                  // The copyright at the beginning of the POT file.
-					potFilename: '',                  // Name of the POT file.
-					potHeaders: {
-						poedit: true,                 // Includes common Poedit headers.
-						'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
-					},                                // Headers to add to the generated POT file.
-					processPot: null,                 // A callback function for manipulating the POT file.
-					type: 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
-					updateTimestamp: true             // Whether the POT-Creation-Date should be updated without other changes.
+					domainPath: 'languages',
+					processPot: function( pot, options ) {
+						pot.headers['report-msgid-bugs-to'] = 'https://themeofthecrop.com';
+						return pot;
+					},
+					type: 'wp-plugin',
 				}
 			}
 		}
